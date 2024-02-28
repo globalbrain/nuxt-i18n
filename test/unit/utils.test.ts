71bf7e50dfc318c5b745d9d3ae59eaec3842702b
Cue <hello@cuebit.dev>
2024-02-28 12:55:50 +0000

    test: remove redundant tests

--- test/unit/utils.test.ts
+++ test/unit/utils.test.ts
@@ -49,61 +49,17 @@ describe('adjustRouteDefinitionForTrailingSlash', () => {
 })
 
 describe('getLocaleRouteName', () => {
-  describe('strategy: prefix_and_default', () => {
-    it('should be `route1___en___default`', () => {
-      assert.equal(
-        getLocaleRouteName('route1', 'en', {
-          defaultLocale: 'en',
-          strategy: 'prefix_and_default',
-          routesNameSeparator: '___',
-          defaultLocaleRouteNameSuffix: 'default',
-        }),
-        'route1___en___default',
-      )
-    })
+  it('should be `route1___en`', () => {
+    assert.equal(
+      getLocaleRouteName('route1', 'en', { routesNameSeparator: '___' }),
+      'route1___en'
+    )
   })
 
-  describe('strategy: prefix_except_default', () => {
-    it('should be `route1___en`', () => {
-      assert.equal(
-        getLocaleRouteName('route1', 'en', {
-          defaultLocale: 'en',
-          strategy: 'prefix_except_default',
-          routesNameSeparator: '___',
-          defaultLocaleRouteNameSuffix: 'default',
-        }),
-        'route1___en',
-      )
-    })
-  })
-
-  describe('strategy: no_prefix', () => {
-    it('should be `route1`', () => {
-      assert.equal(
-        getLocaleRouteName('route1', 'en', {
-          defaultLocale: 'en',
-          strategy: 'no_prefix',
-          routesNameSeparator: '___',
-          defaultLocaleRouteNameSuffix: 'default',
-        }),
-        'route1',
-      )
-    })
-  })
-
-  describe('irregular', () => {
-    describe('route name is null', () => {
-      it('should be ` (null)___en___default`', () => {
-        assert.equal(
-          getLocaleRouteName(null, 'en', {
-            defaultLocale: 'en',
-            strategy: 'prefix_and_default',
-            routesNameSeparator: '___',
-            defaultLocaleRouteNameSuffix: 'default',
-          }),
-          '(null)___en___default',
-        )
-      })
-    })
+  it('should be ` (null)___en`', () => {
+    assert.equal(
+      getLocaleRouteName(null, 'en', { routesNameSeparator: '___' }),
+      '(null)___en'
+    )
   })
 })
